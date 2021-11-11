8a4e1516e3c867f5cb633ab9b5dfddfb0472dd65
Lester Deeley <lester.deeley@yahoo.com>
2021-11-11 12:31:31 +0000

    Add Mockito

---
+++ src/main/java/FirstNonRepeatingCharacter.java
@@ -0,0 +1,11 @@
+public class FirstNonRepeatingCharacter {
+
+    public static String firstNonRepeatingCharacter(String string){
+        String result = "Empty String";
+        if (string.length()<1) return result;
+        if (string.length()==1) return string;
+
+        return result;
+    }
+
+}
