96651946b2a9d230927e72d07e4c1c2d763b1c78
Lester Deeley <lester.deeley@yahoo.com>
2021-11-11 11:10:03 +0000

    Add a Hamcrest test

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Character ch = FirstNonRepeatedCharacter.firstNonRepeatedCharacter("sester");
+        System.out.println(ch);
+    }
+
+}
