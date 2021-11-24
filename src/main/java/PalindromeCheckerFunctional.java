56c7b6cb51a1cacdef979ee3e536e53d253f8187
Lester Deeley <lester.deeley@yahoo.com>
2021-11-24 12:37:31 +0000

    Palindrome checker take 3

--- src/main/java/PalindromeCheckerFunctional.java
+++ src/main/java/PalindromeCheckerFunctional.java
@@ -1,12 +1,15 @@
 import java.util.Locale;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class PalindromeCheckerFunctional {
 
     public static boolean palindromeCheckerFunctional(String str){
 
-        return str.equals(new StringBuilder(str).reverse().toString());
+//        return str.equals(new StringBuilder(str).reverse().toString());
+        return IntStream.range(0,str.length()/2)
+                .noneMatch(p->str.charAt(p)!=str.charAt(str.length()-p-1));
     }
 
 }
