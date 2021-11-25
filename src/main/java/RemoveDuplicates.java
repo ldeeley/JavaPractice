91351fdccadc4f4ed3f7fdfc64108656172a85f6
Lester Deeley <lester.deeley@yahoo.com>
2021-11-25 16:51:06 +0000

    RemoveDuplicates

---
+++ src/main/java/RemoveDuplicates.java
@@ -0,0 +1,17 @@
+import java.util.LinkedHashSet;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+public class RemoveDuplicates {
+
+    public static String removeDuplicates(String str){
+
+        str
+                .chars()
+                .mapToObj(c-> (char) c)
+                .collect(Collectors.toSet(new LinkedHashSet<Character>(), )).t;
+
+        return str;
+    }
+
+}
---
+++ src/test/java/RemoveDuplicatesTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RemoveDuplicatesTest {
+
+    @Test
+    void removeDuplicates() {
+        assertEquals("LeSTR", RemoveDuplicates.removeDuplicates("LeeSTERT"));
+    }
+}
